c6e53494d3e88eafb2b18311dba7f638b30949de
rkmaxhero <rounaqkhan12@gmail.com>
2024-07-27 00:05:47 -0400

    save

---
+++ driver.py
@@ -0,0 +1,24 @@
+import subprocess
+import json
+import os
+
+# THIS SCRIPT WAS REPLACED BY lambda_function.py TO TRANSFER TO AWS
+# run command: python driver.py
+start_urls = [
+    "https://kissmanga.org/manga/manga-bc979159",
+    "https://kissmanga.org/manga/manga-ny991307",
+    #"https://kissmanga.org/manga/manga-ln951470",
+    #"https://kissmanga.org/manga/manga-oh991742"
+]
+
+#def driverFunc(start_urls):
+
+start_urls_json = json.dumps(start_urls)
+
+os.environ['START_URLS_JSON'] = start_urls_json
+
+# Run the scrapy spider command for manga crawler 
+subprocess.run(["scrapy", "crawl", "manga", "-o", "manga.json"], check=True)
+
+# Run the sorting Python script
+subprocess.run(["python", "sorting.py"], check=True)
