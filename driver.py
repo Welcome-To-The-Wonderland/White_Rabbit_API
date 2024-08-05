99d3cbf559ac32e66c17b5587fd95145c75336ef
rkmaxhero <rounaqkhan12@gmail.com>
2024-08-05 19:29:06 -0400

    save comments

--- driver.py
+++ driver.py
@@ -4,11 +4,16 @@ import os
 
 # THIS SCRIPT WAS REPLACED BY lambda_function.py TO TRANSFER TO AWS
 # run command: python driver.py
+
+# HEY ROUNAQ LOOK HERE 
+# YOU NEED TO MAKE IT SUCH THAT THIS WORKS FROM AN INPUT OF A JSON ARRAY OF URLS
+# NEXT, CHANGE YOUR LAMBDA FUNCTION NAMED "EC2_script_runner" TO TAKE IN A LIST FROM ... somewhere
+#   - DUDE FIGURE WHERE THE HELL WOULD BE BEST PRACTICE TO GET THIS LIST FROM
 start_urls = [
     "https://kissmanga.org/manga/manga-bc979159",
     "https://kissmanga.org/manga/manga-ny991307",
-    "https://kissmanga.org/manga/manga-oh991742"
-    #"https://kissmanga.org/manga/manga-ln951470",
+    "https://kissmanga.org/manga/manga-oh991742",
+    "https://kissmanga.org/manga/manga-ln951470",
 ]
 
 #def driverFunc(start_urls):
